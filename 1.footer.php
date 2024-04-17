33da75001a7ce7023e7a92933e233127bf70fd4e
Abdulrahaman27 <132663867+Abdulrahaman27@users.noreply.github.com>
2024-04-17 10:32:13 -0700

    Add files via upload
    Added the files I worked on.

---
+++ 1.footer.php
@@ -0,0 +1,33 @@
+<!--**********************************
+    Footer start
+***********************************-->
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">DexignLab</a>
+            2024</p>
+    </div>
+</div>
+<!--**********************************
+    Footer end
+***********************************-->
+
+</div>
+<script src="public/assets/vendor/global/global.min.js"></script>
+<script src="public/assets/vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+
+
+
+<script src="public/assets/js/custom.min.js"></script>
+<script src="public/assets/js/dlabnav-init.js"></script>
+<script src="public/assets/js/demo.js"></script>
+<script src="public/assets/js/styleSwitcher.js"></script>
+
+
+<!--**********************************
+        Main wrapper end
+    ***********************************-->
+</body>
+
+<!-- Mirrored from travl.dexignlab.com/codeigniter/demo/page-empty by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 05 Mar 2024 15:48:03 GMT -->
+
+</html>
